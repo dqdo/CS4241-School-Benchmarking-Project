6717eabd679e5f1976912f2478aea82f212a309b
dqdo <dqdo873@gmail.com>
2026-02-19 18:06:42 -0500

    Added NavBar

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,51 +1,11 @@
-import "./App.css";
-
-import {useEffect, useState} from "react";
-
-import reactLogo from "./assets/react.svg";
+import NavBar from "./components/NavBar";
 
 function App() {
-  const [test1, setTest1] = useState("");
-  const [test2, setTest2] = useState("");
-  const [loggedIn, setLoggedIn] = useState(false);
-  useEffect(() => {
-      fetch("/loggedIn").then(res => res.json()).then(data => setLoggedIn(data.status));
-      fetch("/admin/test").then(res => res.status === 200 ? res.json() : "").then(data => setTest1(data || ""));
-      fetch("/test1").then(res => res.status === 200 ? res.json() : "").then(data => setTest2(data || ""));
-  }, [])
-
-    const getBaseUrl = () => {
-        const protocol = window.location.protocol;
-        const host = window.location.host; // includes the hostname and optional port
-        const baseUrl = `${protocol}//${host}`;
-        return baseUrl;
-    };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>Text will appear if logged in as Admin: {test1}</p>
-        <p>Text will appear if you are logged in: {test2}</p>
-      </div>
-        <div hidden={loggedIn}>
-            <a href={getBaseUrl() + "/login"}>Log In</a>
-        </div>
-        <div hidden={!loggedIn}>
-            <a href={getBaseUrl() + "/logout"}>Logout</a>
-        </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+      <>
+          <NavBar/>
+      </>
   );
 }
 
---
+++ src/client/components/ButtonTab.tsx
@@ -0,0 +1,5 @@
+function ButtonTab() {
+
+}
+
+export default ButtonTab;
